7a527279bc5ab577b7b2cce94ef2f6a346e15bb0
ZJ <1935662588@qq.com>
2021-05-04 00:03:11 +0800

    大疆CAN协议电机驱动代码

---
+++ MDK-ARM/RTE/_3508_MOTOR/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '3508_MOTOR' 
+ * Target:  '3508_MOTOR' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
